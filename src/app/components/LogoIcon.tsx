4aafed2845445f92027407de4ad5d71ccd1ffd0b
Vakiyama <vakiyama@outlook.com>
2024-11-25 12:29:09 -0800

    do thing

---
+++ src/app/components/LogoIcon.tsx
@@ -0,0 +1,177 @@
+export function LogoIcon() {
+  return (
+    <svg viewBox="0 0 322 413" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_i_29_73)">
+        <path
+          d="M322.001 136.125C322.001 -45.375 9.53674e-06 -45.375 0 136.125C-9.53674e-06 306.125 163 412.125 163 412.125C163 412.125 322.001 310.625 322.001 136.125Z"
+          fill="#4F3D65"
+        />
+      </g>
+      <g filter="url(#filter1_d_29_73)">
+        <circle cx="161" cy="156" r="91" fill="#A0BAAA" />
+        <circle
+          cx="161"
+          cy="156"
+          r="93.1065"
+          stroke="#393939"
+          strokeWidth="4.21296"
+        />
+      </g>
+      <g filter="url(#filter2_d_29_73)">
+        <path
+          d="M188.384 183.384C188.384 202.464 172.917 217.931 153.838 217.931C134.759 217.931 119.292 202.464 119.292 183.384C119.292 164.305 134.759 148.838 153.838 148.838C155.435 148.838 156.267 149.339 156.982 150.141C157.903 151.174 158.728 152.861 159.611 155.452C160.003 156.6 160.368 157.804 160.766 159.113C160.804 159.238 160.842 159.363 160.88 159.489C161.312 160.911 161.783 162.443 162.32 163.955C163.38 166.936 164.827 170.224 167.193 172.944C168.953 174.967 171.31 175.714 173.162 176.017C175.026 176.321 177.005 176.274 178.671 176.185C179.493 176.141 180.195 176.092 180.852 176.046C181.733 175.984 182.534 175.927 183.437 175.898C184.91 175.849 185.82 175.925 186.349 176.066C186.835 176.196 187.28 176.454 187.711 177.674C188.204 179.071 188.384 181.037 188.384 183.384Z"
+          stroke="white"
+          strokeWidth="9.26852"
+        />
+        <rect
+          x="145.096"
+          y="121.454"
+          width="27.8056"
+          height="59.8241"
+          rx="6.74074"
+          fill="white"
+        />
+        <rect
+          x="191.438"
+          y="181.278"
+          width="45.5"
+          height="11.7963"
+          rx="5.89815"
+          transform="rotate(-180 191.438 181.278)"
+          fill="white"
+        />
+        <rect
+          x="207.394"
+          y="203.889"
+          width="40.438"
+          height="11.7963"
+          rx="5.89815"
+          transform="rotate(-135 207.394 203.889)"
+          fill="white"
+        />
+        <rect
+          x="118.876"
+          y="99.5463"
+          width="45.5"
+          height="11.7963"
+          rx="5.89815"
+          transform="rotate(34.8845 118.876 99.5463)"
+          fill="white"
+        />
+        <rect
+          width="45.5"
+          height="11.7963"
+          rx="5.89815"
+          transform="matrix(-0.820306 0.571925 0.571925 0.820306 199.272 99.5463)"
+          fill="white"
+        />
+        <circle cx="159.42" cy="104.602" r="14.3241" fill="white" />
+      </g>
+      <defs>
+        <filter
+          id="filter0_i_29_73"
+          x="0"
+          y="0"
+          width="322.001"
+          height="412.125"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="-14" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.67451 0 0 0 0 0.627451 0 0 0 0 0.733333 0 0 0 1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="shape"
+            result="effect1_innerShadow_29_73"
+          />
+        </filter>
+        <filter
+          id="filter1_d_29_73"
+          x="62.838"
+          y="60.787"
+          width="203.065"
+          height="203.486"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="3.37037" dy="6.74074" />
+          <feGaussianBlur stdDeviation="3.15972" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_29_73"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_29_73"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter2_d_29_73"
+          x="114.443"
+          y="90.2778"
+          width="107.275"
+          height="140.713"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="5.05556" dy="5.05556" />
+          <feGaussianBlur stdDeviation="1.68519" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_29_73"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_29_73"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
